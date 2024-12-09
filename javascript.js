a9006df92fd837a38f4704b2699dc96456b9d8fc
Corey Gray <16312666+csgray@users.noreply.github.com>
2024-12-09 17:25:22 -0500

    feat: Add form to add book to the library
    It's ugly but it works.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,18 @@
+// Elements and variables
+const library = document.getElementById("library");
+const addBook = document.getElementById("add-book");
 const myLibrary = [];
 
 // Constructor function (Legacy)
-function Book(title, author, pages, read) {
+function Book(title, author, pages = 0, read = false) {
     if (!new.target) {
-        throw Error('Must use the new operator to call the function');
+        throw Error("Must use the new operator to call the function");
     }
 
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.read = Boolean(read);
 }
 
 // Set the function on the prototype and not on the constructor
@@ -42,51 +45,63 @@ function getRandomColor() {
     return allowedColors[Math.floor(Math.random() * allowedColors.length)];
 }
 
+function displayBook(book) {
+    const newBook = document.createElement("div");
+    newBook.classList.add("book");
+    newBook.style.setProperty("background-color", getRandomColor());
+    newBook.style.setProperty("color", "white");
+
+    // Title
+    const title = document.createElement("div");
+    title.classList.add("title");
+    title.textContent = book.title;
+    newBook.appendChild(title);
+
+    // Author
+    const author = document.createElement("div");
+    author.classList.add("author");
+    author.textContent = book.author;
+    newBook.appendChild(author);
+
+    // Pages
+    const pages = document.createElement("div");
+    pages.classList.add("pages");
+    pages.textContent = `${book.pages}p`;
+    newBook.appendChild(pages);
+
+    // Read
+    const read = document.createElement("div");
+    read.classList.add("read");
+    if (book.read) {
+        read.textContent = "read";
+    } else {
+        read.textContent = "not read";
+    }
+    newBook.appendChild(read);
+
+    library.appendChild(newBook);
+}
+
 function displayBooks() {
-    const library = document.getElementById('library');
-
     myLibrary.forEach((book) => {
-        const newBook = document.createElement("div");
-        newBook.classList.add("book");
-        newBook.style.setProperty("background-color", getRandomColor());
-        newBook.style.setProperty("color", "white");
-
-        // Title
-        const title = document.createElement("div");
-        title.classList.add("title");
-        title.textContent = book.title;
-        newBook.appendChild(title);
-
-        // Author
-        const author = document.createElement("div");
-        author.classList.add("author");
-        author.textContent = book.author;
-        newBook.appendChild(author);
-
-        // Pages
-        const pages = document.createElement("div");
-        pages.classList.add("pages");
-        pages.textContent = `${book.pages}p`;
-        newBook.appendChild(pages);
-
-        // Read
-        const read = document.createElement("div");
-        read.classList.add("read");
-        if (book.read) {
-            read.textContent = "read";
-        } else {
-            read.textContent = "not read";
-        }
-        newBook.appendChild(read);
-
-        library.appendChild(newBook);
+        displayBook(book);
     });
 }
 
-// Execution
+addBook.addEventListener("submit", (event) => {
+    // Intercept the form submittal and datal
+    event.preventDefault();
+    const formData = new FormData(addBook);
+    entries = Object.fromEntries(formData);
 
+    addBookToLibrary(entries.title, entries.author, entries.pages, entries.read);
+    displayBook(myLibrary.at(-1)); // The just added book is at the end of the array.
 
+    // Clear the inputs to add another book
+    addBook.reset();
+});
 
+// Example data
 addBookToLibrary("Mere Christianity", "C.S. Lewis", "227", true);
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "384", true);
 addBookToLibrary("The Fellowship of the Ring", "J.R.R. Tolkien", "480", true);
@@ -95,5 +110,5 @@ addBookToLibrary("The Return of the King", "J.R.R. Tolkien", "512", true);
 addBookToLibrary("The Last Unicorn", "Peter S. Beagle", "320", true);
 addBookToLibrary("The Way Home", "Peter S. Beagle", "224", false);
 
-console.log(myLibrary)
+// Execution
 displayBooks();
